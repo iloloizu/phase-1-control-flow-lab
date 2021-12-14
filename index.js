d44e1011e92ed778d55a980691928b1612fea8e4
Ilolo Izu <iloloizu97@gmail.com>
2021-12-13 21:08:47 -0600

    commit tree 324e0ac7a62017a885752d9746d8f336054be5df

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue >= 2000 && someValue < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValue > 2500) {
+    return "No can do."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName) {
+  return (cityName === "NYC" ? "Ok, sounds good." : 'No go.')
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(moreMoney) {
+  switch (moreMoney) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    case 'thanks for everything':
+      return "Bye."
+  }
   // Write your code here!
 }
